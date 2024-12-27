69f2c73e7ef8332189b162725bdb4a4d4d0851b9
foreverchild <harrisonogbonnaya@gmail.com>
2024-12-27 21:47:00 +0100

    added cores config

---
+++ src/main/java/com/foreverchild/paystack_integration/utils/WebConfig.java
@@ -0,0 +1,26 @@
+package com.foreverchild.paystack_integration.utils;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final String[] allowedOrigins;
+
+    public WebConfig() {
+        Dotenv dotenv = Dotenv.load();
+        this.allowedOrigins = dotenv.get("ALLOWED_ORIGINS", "http://localhost:9091").split(",");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
